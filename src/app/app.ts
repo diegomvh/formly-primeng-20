8935bdf3fbe89432e74d9fb49be4180a6e6dda55
Diego van Haaster <diegomvh@gmail.com>
2025-08-19 13:46:18 -0300

    Test

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,78 +17,159 @@ export class App {
   model = {};
   fields: FormlyFieldConfig[] = [
     {
-      key: 'input',
-      type: 'input',
-      className: 'col-4',
+      key: 'autocomplete',
+      type: 'autocomplete',
+      className: 'col-12',
       templateOptions: {
-        label: 'Input',
-        placeholder: 'Input placeholder',
-        required: true,
+        label: 'AutoComplete',
+        placeholder: 'Autocomplete placeholder',
       },
     },
     {
-      key: 'inputtext',
-      type: 'inputtext',
-      className: 'col-4',
+      key: 'cascadeselect',
+      type: 'cascadeselect',
+      className: 'col-12',
       templateOptions: {
-        label: 'InputText',
-        placeholder: 'Input placeholder',
-        required: true,
-      },
-    },
-    {
-      key: 'inputotp',
-      type: 'inputotp',
-      className: 'col-4',
-      templateOptions: {
-        label: 'InputOtp',
-        placeholder: 'InputOtp placeholder',
-        required: true,
-      },
-    },
-    {
-      key: 'inputnumber',
-      type: 'inputnumber',
-      className: 'col-4',
-      templateOptions: {
-        label: 'InputNumber',
-        placeholder: 'InputNumber placeholder',
-        required: true,
-      },
-    },
-    {
-      key: 'inputmask',
-      type: 'inputmask',
-      className: 'col-4',
-      templateOptions: {
-        label: 'InputMask',
-        placeholder: 'InputMask placeholder',
-        required: true,
+        label: 'CascadeSelect',
+        placeholder: 'CascadeSelect placeholder',
       },
     },
     {
       key: 'checkbox',
       type: 'checkbox',
-      className: 'col-2',
+      className: 'col-12',
       templateOptions: {
         label: 'Checkbox',
         binary: true,
       },
     },
     {
-      key: 'datepicker',
-      type: 'datepicker',
-      className: 'col-2',
+      key: 'colorpicker',
+      type: 'colorpicker',
+      className: 'col-12',
       templateOptions: {
-        label: 'DatePicker',
+        label: 'ColorPicker',
+        placeholder: 'ColorPicker placeholder',
       },
     },
     {
-      key: 'colorpicker',
-      type: 'colorpicker',
-      className: 'col-2',
+      key: 'datepicker',
+      type: 'datepicker',
+      className: 'col-12',
       templateOptions: {
-        label: 'ColorPicker',
+        label: 'DatePicker',
+        placeholder: 'DatePicker placeholder',
+      },
+    },
+    {
+      key: 'editor',
+      type: 'editor',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Editor',
+        placeholder: 'Editor placeholder',
+      },
+    },
+    {
+      key: 'inputmask',
+      type: 'inputmask',
+      className: 'col-12',
+      templateOptions: {
+        label: 'InputMask',
+        placeholder: 'InputMask placeholder',
+      },
+    },
+    {
+      key: 'inputnumber',
+      type: 'inputnumber',
+      className: 'col-12',
+      templateOptions: {
+        label: 'InputNumber',
+        placeholder: 'InputNumber placeholder'
+      },
+    },
+    {
+      key: 'inputotp',
+      type: 'inputotp',
+      className: 'col-12',
+      templateOptions: {
+        label: 'InputOtp',
+        placeholder: 'InputOtp placeholder'
+      },
+    },
+    {
+      key: 'input',
+      type: 'input',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Input',
+        placeholder: 'Input placeholder'
+      },
+    },
+    {
+      key: 'inputtext',
+      type: 'inputtext',
+      className: 'col-12',
+      templateOptions: {
+        label: 'InputText',
+        placeholder: 'InputText placeholder',
+      },
+    },
+    {
+      key: 'knob',
+      type: 'knob',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Knob',
+        placeholder: 'Knob placeholder',
+      },
+    },
+    {
+      key: 'listbox',
+      type: 'listbox',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Listbox',
+        placeholder: 'Listbox placeholder',
+      },
+    },
+    {
+      key: 'multiselect',
+      type: 'multiselect',
+      className: 'col-12',
+      templateOptions: {
+        label: 'MultiSelect',
+        placeholder: 'MultiSelect placeholder',
+      },
+    },
+    {
+      key: 'password',
+      type: 'password',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Password',
+        placeholder: 'Password placeholder',
+      },
+    },
+    {
+      key: 'radiobutton',
+      type: 'radiobutton',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Radio',
+        options: [
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+        ],
+      },
+    },
+    {
+      key: 'rating',
+      type: 'rating',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Rating',
+        placeholder: 'Rating placeholder',
       },
     },
     {
@@ -107,16 +188,27 @@ export class App {
       },
     },
     {
-      key: 'radio',
-      type: 'radiobutton',
+      key: 'selectbutton',
+      type: 'selectbutton',
       className: 'col-2',
       templateOptions: {
-        label: 'Radio',
+        label: 'SelectButton',
+        placeholder: 'SelectButton placeholder',
         required: true,
         options: [
           { label: 'Option 1', value: '1' },
           { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
         ],
+      },
+    },
+    {
+      key: 'slider',
+      type: 'slider',
+      className: 'col-12',
+      templateOptions: {
+        label: 'Slider',
+        placeholder: 'Slider placeholder'
       },
     },
     {
@@ -126,59 +218,26 @@ export class App {
       templateOptions: {
         label: 'TextArea',
         placeholder: 'Textarea placeholder',
-        required: true,
       },
     },
     {
-      key: 'password',
-      type: 'password',
+      key: 'togglebutton',
+      type: 'togglebutton',
       className: 'col-12',
       templateOptions: {
-        label: '',
-        placeholder: 'Password placeholder',
-        required: true,
+        label: 'ToggleButton',
+        placeholder: 'ToggleButton placeholder',
       },
     },
     {
-      key: 'slider',
-      type: 'slider',
+      key: 'toggleswitch',
+      type: 'toggleswitch',
       className: 'col-12',
       templateOptions: {
-        label: '',
-        placeholder: 'Slider placeholder',
-        required: true,
+        label: 'ToggleSwitch',
+        placeholder: 'ToggleSwitch placeholder',
       },
     },
-    {
-      key: 'knob',
-      type: 'knob',
-      className: 'col-12',
-      templateOptions: {
-        label: '',
-        placeholder: 'Knob placeholder',
-        required: true,
-      },
-    },
-    {
-      key: 'editor',
-      type: 'editor',
-      className: 'col-12',
-      templateOptions: {
-        label: '',
-        placeholder: 'Editor placeholder',
-        required: true,
-      },
-    },
-    {
-      key: 'autocomplete',
-      type: 'autocomplete',
-      className: 'col-12',
-      templateOptions: {
-        label: 'AutoComplete',
-        placeholder: 'Autocomplete placeholder',
-        required: true,
-      },
-    }
   ];
 
   onSubmit() {
